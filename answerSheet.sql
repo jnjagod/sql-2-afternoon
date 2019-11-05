7003857acdbffd6d72b0d26e465d1299ebf619b1
jnjagod <josh.jagoda@gmail.com>
2019-11-05 13:50:27 -0800

    complet

--- answerSheet.sql
+++ answerSheet.sql
@@ -12,7 +12,7 @@ join customer c on i.customer_id = c.customer_id;
 from customer c
 join employee e on c.support_rep_id = e.employee_id;
 
-4. select a.name,  al.title from album al
+4. select a.name, al.title from album al
 join artist a on a.artist_id = al.artist_id;
 
 5. select pl.track_id from playlist_track pl
@@ -49,7 +49,7 @@ where genre_id in (select genre_id from genre where name = 'Comedy');
 5. select * from track
 where album_id in (select album_id from album where title = 'Fireball');
 
-6. select *from track
+6. select * from track
 where album_id in (select album_id from album where artist_id in (select artist_id from artist where name = 'Queen'));
 
 Updating Rows
@@ -85,9 +85,9 @@ join genre g on g.genre_id = t.genre_id
 where g.name = 'Pop' or g.name = 'Rock'
 group by g.name;
 
-3. select ar.name, count(*) from album al
-join artist ar on ar.artist_id = al.artist_id
-group by ar.name;
+3. select a.name, count(*) from album al
+join artist a on a.artist_id = al.artist_id
+group by a.name;
 
 Distinct
 
@@ -101,6 +101,40 @@ Delete Rows
 
 2. delete from practice_delete where type = 'bronze';
 
-3. delete from practice_delete where type = 'silver'
+3. delete from practice_delete where type = 'silver';
 
-4. delete from prectice_delete where value = 150
+4. delete from prectice_delete where value = 150;
+
+E-Commerce
+
+create table users (user_id serial primary key, name varchar(20), email varchar(30));
+
+create table products (product_id serial primary key, name varchar(40), price numeric(9,2));
+
+create table orders (order_id serial primary key, product_id int references products(product_id));
+
+insert into users (name, email)
+values ('Jim', 'jim@email.com'),('Sam', 'sam@email.com'),('Bob', 'bob@email.com');
+
+insert into products (name, price)
+values ('cheese wheel', 99.99), ('sandals', 61.99),('vacuum', 120.65);
+
+insert into orders (product_id) values (1),(2),(3);
+
+select * from orders where order_id = 1;
+
+select * from orders;
+
+select sum(price) from orders o join products p on p.product_id = o.product_id where order_id = 1;
+
+alter table orders add column user_id int references users(user_id);
+
+update orders set user_id = 3 where order_id = 1;
+
+update orders set user_id = 2 where order_id = 3;
+
+update orders set user_id = 1 where order_id = 2;
+
+alter table orders add column qty int;
+
+insert into orders (product_id, user_id, qty) values (3,3,4), (2,3,1), (1,1,7), (2,2,2), (3,2,5);
